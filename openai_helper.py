f5156b8f1a3ca987d8676639429df41ab7b3341c
Shuli Kang <KSL777@protonmail.com>
2024-02-07 14:02:28 -0800

    Update GPT model versions

--- openai_helper.py
+++ openai_helper.py
@@ -1,8 +1,7 @@
 import os
 
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 DEFAULT_TEMP = 0.7
 GPT3 = "gpt-3.5-turbo-16k"
 GPT4 = "gpt-4-turbo-128k"
 
-MODEL_DICT = {GPT3 : "gpt-3.5-turbo-1106", GPT4 : "gpt-4-1106-preview"}
+MODEL_DICT = {GPT3: "gpt-3.5-turbo-0125", GPT4: "gpt-4-turbo-preview"}
